8125a71dada088dcf8b6fb68aecfc6e213790578
Leonardo <leolou.90@gmail.com>
2022-07-10 19:18:36 -0700

    basic html sass files

---
+++ index.js
@@ -0,0 +1,22 @@
+let menu = document.getElementById('menu');
+let menuDiv = document.getElementById('link_div');
+let links = document.getElementById('page_links');
+let isShown = false;
+
+
+
+menu.addEventListener("click", showMenu);
+links.addEventListener('click', showMenu);
+
+
+function showMenu(){
+
+    if(isShown === false){
+        menuDiv.style.top = "100%";
+        isShown = true;
+    }else if(isShown === true){
+        menuDiv.style.top = "-600%";
+        isShown = false;
+    }
+    
+}
